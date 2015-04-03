55b4eb28010a16c1e90889b3ab9d056e25fc7eb9
Yogthos <dmitri.sotnikov@gmail.com>
2015-04-03 13:04:37 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject alpha-id "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject alpha-id "0.1"
+  :description "a library for convering numbers into short alphanumeric IDs"
+  :url "https://github.com/yogthos/alpha-id"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
